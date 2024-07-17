508152337c4bcac0a04ee593c625e9812c16c1d9
Lucio Daou <53634214+luciodaou@users.noreply.github.com>
2024-07-17 17:13:46 -0300

    Fedora Deps

---
+++ deps.sh
@@ -0,0 +1,12 @@
+# lz4jsoncat
+
+sudo dnf install lz4-devel lz4-libs lsb_release
+
+cd ~/Downloads
+git clone https://github.com/andikleen/lz4json
+cd lz4json
+make
+sudo mv lz4jsoncat /usr/local/bin
+
+
+
